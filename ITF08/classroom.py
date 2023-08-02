67b0ed2bcc3d1915ace7f0a71ed9087411ca2d0c
Karam1000 <140180956+Karam1000@users.noreply.github.com>
2023-08-02 16:47:07 +0200

    ID

---
+++ ITF08/classroom.py
@@ -0,0 +1,15 @@
+x = int(input("Enter number1"))
+y = int(input("Enter numbe 2"))
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply (x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+print (add(x,y))
+print (subtract(x,y))
+print(divide(x,y))
+print(multiply(x,y))
+
